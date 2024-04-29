3a9021a6c56f34b7aef1c97fa43d72ecf5771e80
hamza <hamzakhalem97@gmail.com>
2024-04-29 20:06:27 +0100

    new dadta

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for core project.
+"""core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from .views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", index, name="index"),
+    path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    # setting this to view media files from admin panel
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
